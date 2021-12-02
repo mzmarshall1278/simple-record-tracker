a98813300cd28c52857ed7181a4019ad85c1c5e6
MZ Marshall <mzmarshall1278@gmail.com>
2021-12-02 18:57:39 +0100

    created table

---
+++ components/AppTable.vue
@@ -0,0 +1,40 @@
+<template>
+    <table class="w-full bg-red-100">
+        <tr>
+            <th class="">
+                <td v-for="(col, key) in columns" :key="key">{{col.label}}</td>
+            </th>                    
+        </tr>
+        <tr v-for="(row,key) in data" :key="key">
+            <tbody>
+                <td v-for="(item, key) in columns" :key="key">{{row[item.value]}}</td>
+            </tbody>
+        </tr>
+    </table>
+</template>
+r v-for="(row,key) in data" :key="key">
+            <tbody>
+                <td v-for="(item, key) in columns" :key="key">{{row[item.value]}}</td>
+            </tbody>
+        </tr>
+<script>
+export default {
+    props: {
+        columns: {
+            type: Array
+        },
+        data: {
+            type: Array
+        },
+    }
+
+}
+</script>
+<style>
+table tbody tr:nth-child(2n+1) {
+    @apply bg-blue-50 
+  }
+  td {
+      @apply text-center w-auto
+  }
+</style>
--- pages/Sellers.vue
+++ pages/Sellers.vue
@@ -1,26 +1,40 @@
 <template>
   <div>
     <div class="text-5xl font-black text-blue-500 text-center mt-12">Vendors</div>
-    <div class="w-10/12 mx-auto mt-4 grid md:grid-cols-2 lg:grid-cols-3 gap-5">
-    <Card :title="stock.title" :rows="stock.rows" />
-    <Card :title="pending.title" :rows="pending.rows" />
-    <Card :title="free.title" :rows="free.rows" />
+    <div class="w-10/12 mx-auto">
+      <AppTable
+      :columns="columns"
+      :data="vendors"
+    />
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
-export default { 
-  data(){
-    return {
-      page: 1,
-    }
-  },
-  methods: {
-    getVendors(page){
-      axios.get(`http://localhost:3000/vendors?page=${page}&`)
-    }
-  }
+export default {
+    data() {
+        return {
+          columns: [
+            {value: 'name', label: 'Name'},
+            {value: 'address', label: 'Address'},
+            {value: 'LGA', label: 'LGA'},
+            {value: 'phone', label: 'Phone Number'},
+            {value: 'deal', label: 'Deal'},
+          ],
+          vendors: [],
+            page: 1,
+        };
+    },
+    methods: {
+        getVendors(page) {
+            axios.get(`http://localhost:3000/vendors?page=${page}&`).then(res => {
+                this.vendors = res.data
+            });
+        }
+    },
+    mounted() {
+        this.getVendors('1');
+    },
 }
 </script>
