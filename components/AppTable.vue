84e1c0e8e6e75286ad5c45043f726efea6d10525
MZ Marshall <mzmarshall1278@gmail.com>
2021-12-04 23:55:59 +0100

    Added tailwind.confug.js and made table to accept links

--- components/AppTable.vue
+++ components/AppTable.vue
@@ -7,15 +7,15 @@
                 </tr>
             </thead>
             <tbody>
-            <tr v-for="(row,key) in data" :key="key">
+            <tr v-for="(row,key) in data" :key="key" @click="open(row)">
                 <td v-for="(item, key) in columns" :key="key">{{row[item.value]}}</td>
             </tr>
             </tbody>
         </table>
         <div class="flex w-auto mx-auto ">
-            <button class="bg-blue-100 text-blue-800 py-4 px-8 rounded-lg mx-4 hover:bg-blue-500 hover:text-white"> &lt;</button>
-            <button class="bg-blue-100 text-blue-800 py-4 px-8 rounded-lg mx-4 hover:bg-blue-500 hover:text-white" v-for="(page, index) in pages" :key="index">{{index+1}}</button>
-            <button class="bg-blue-100 text-blue-800 py-4 px-8 rounded-lg mx-4 hover:bg-blue-500 hover:text-white">&gt;</button>
+            <button :disabled="page == 1" @click="previous" class="bg-blue-100 text-blue-800 py-4 px-8 rounded-lg mx-4 hover:bg-blue-500 hover:text-white disabled:opacity-0 disabled:cursor-not-allowed"> &lt;</button>
+            <button @click="changePage(index)" class="bg-blue-100 text-blue-800 py-4 px-8 rounded-lg mx-4 hover:bg-blue-500 hover:text-white" v-for="(page, index) in pages" :key="index">{{index+1}}</button>
+            <button  :disabled="page == pages" @click="next" class="bg-blue-100 text-blue-800 py-4 px-8 rounded-lg mx-4 hover:bg-blue-500 hover:text-white disabled:opacity-0">&gt;</button>
         </div>
     </div>
 </template>
@@ -31,6 +31,12 @@ export default {
         objectCount: {
             type: Number,
             default: 28
+        },
+        link: {
+            type: Object,
+            default(){
+                return {path: '/sellers/', id: 'phone'}
+            }
         }
     },
     data() {
@@ -41,6 +47,20 @@ export default {
     computed: {
         pages(){
             return Math.ceil(this.objectCount/10);
+        },
+    },
+    methods: {
+        previous(){
+            console.log('previous');
+        },
+        next(){
+            console.log('next');
+        },
+        changePage(index){
+            console.log(index);
+        },
+        open(row){
+            console.log(`${this.link.path}${row[this.link.id]}`);
         }
     }
 
@@ -58,5 +78,8 @@ table tbody tr:nth-child(2n+1) {
   }
   tr:hover {
       @apply bg-blue-100 text-blue-900 cursor-pointer 
+  }
+  .disabled{
+      @apply border-4 border-solid
   }
 </style>
