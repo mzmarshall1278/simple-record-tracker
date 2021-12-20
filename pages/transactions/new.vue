f01f86cafdd00bf50124a680b8c8d1c64c6f0141
MZ Marshall <mzmarshall1278@gmail.com>
2021-12-20 23:56:46 +0100

    started add transaction form

---
+++ pages/transactions/new.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div class="text-5xl font-black text-blue-500 text-center mt-12">Add NewTransactions</div>
+        <form @submit.prevent="addNew">
+            <div class="w-2/5 text-center mx-auto py-6 px-8 mt-4">
+                <div class="mt-8">
+                <input placeholder="Full Name" name="name" type="text" class="w-full py-4 pl-3 border-3 border-blue-500">
+                </div>
+                <div class="mt-8">
+                    <input placeholder="Phone Number" name="name" type="text" class="w-full py-4 pl-3 border-3 border-blue-500">
+                </div>
+                <div class="mt-8">
+                    <textarea placeholder="Address" name="name" type="text" class="w-full py-4 pl-3 border-3 border-blue-500"></textarea>
+                </div>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            addNew(){
+
+            }
+        }
+    }
+</script>
