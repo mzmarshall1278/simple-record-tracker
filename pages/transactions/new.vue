04df2d88dc358a7ce85cd0d3e2762a655d3db13d
MZ Marshall <mzmarshall1278@gmail.com>
2021-12-23 22:32:32 +0100

    Added form fields

--- pages/transactions/new.vue
+++ pages/transactions/new.vue
@@ -2,15 +2,18 @@
     <div>
         <div class="text-5xl font-black text-blue-500 text-center mt-12">Add New Transactions</div>
         <form @submit.prevent="addNew">
-            <div class="w-2/5 text-center mx-auto py-6 px-8 mt-4">
+            <div class="lg:w-2/5 text-center mx-auto py-6 px-8 mt-4 bg-blue-50 rounded-xl">
                 <div class="mt-8">
-                <input placeholder="Full Name" name="name" type="text" class="w-full py-4 pl-3 border-3 border-blue-500">
+                <input name="name" type="date" class="w-full py-4  px-6 pl-3 border-3 border-blue-500 text-gray-400">
                 </div>
                 <div class="mt-8">
-                    <input placeholder="Phone Number" name="name" type="text" class="w-full py-4 pl-3 border-3 border-blue-500">
+                    <input placeholder="Weight" name="name" type="number" class="w-full py-4 pl-3 border-3 border-blue-500">
                 </div>
                 <div class="mt-8">
-                    <textarea placeholder="Address" name="name" type="text" class="w-full py-4 pl-3 border-3 border-blue-500"></textarea>
+                    <input placeholder="price" name="name" type="number" class="w-full py-4 pl-3 border-3 border-blue-500"/>
+                </div>
+                <div class="mt-8">
+                    <input placeholder="Quantity" name="name" type="number" class="w-full py-4 pl-3 border-3 border-blue-500"/>
                 </div>
             </div>
         </form>
