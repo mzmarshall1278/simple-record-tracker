31ddd600320bfe47ac573a285589516d22c934bf
MZ Marshall <mzmarshall1278@gmail.com>
2021-12-23 23:10:08 +0100

    add submit method for transaction

--- pages/transactions/new.vue
+++ pages/transactions/new.vue
@@ -4,17 +4,23 @@
         <form @submit.prevent="addNew">
             <div class="lg:w-2/5 text-center mx-auto py-6 px-8 mt-4 bg-blue-50 rounded-xl">
                 <div class="mt-8">
-                <input name="name" type="date" class="w-full py-4  px-6 pl-3 border-3 border-blue-500 text-gray-400">
+                <input name="name" type="date" v-model="date" class="w-full py-4  px-6 pl-3 border-3 border-blue-500 text-gray-400">
                 </div>
                 <div class="mt-8">
-                    <input placeholder="Weight" name="name" type="number" class="w-full py-4 pl-3 border-3 border-blue-500">
+                    <input placeholder="Weight" name="name" v-model="weight" type="number" class="w-full py-4 pl-3 border-3 border-blue-500">
                 </div>
                 <div class="mt-8">
-                    <input placeholder="price" name="name" type="number" class="w-full py-4 pl-3 border-3 border-blue-500"/>
+                    <input placeholder="price" name="name" v-model="price" type="number" class="w-full py-4 pl-3 border-3 border-blue-500"/>
                 </div>
                 <div class="mt-8">
-                    <input placeholder="Quantity" name="name" type="number" class="w-full py-4 pl-3 border-3 border-blue-500"/>
+                    <input placeholder="Quantity" name="name" v-model="quantity" type="number" class="w-full py-4 pl-3 border-3 border-blue-500"/>
                 </div>
+                <!-- <div class="mt-8">
+                    <button>Search Seller</button>
+                </div> -->
+            </div>
+            <div>
+                <button type="submit">Save Transaction</button>
             </div>
         </form>
     </div>
@@ -22,10 +28,20 @@
 
 <script>
     export default {
+        data(){
+            return {
+                date: '',
+                weight: '',
+                price: '',
+                quantity: '',
+                seller: ''
+            }
+        },
         methods: {
             addNew(){
-
+                const transactionDetails = {date: this.date, weight: +this.weight, price: +this.price, quantity: +this.quantity, seller: this.seller}
+                console.log(transactionDetails);
             }
         }
     }
-</script>
+</script>, 
