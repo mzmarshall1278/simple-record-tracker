8ba612f24a1a912c380c4d702dee161e772b5c91
MZ Marshall <mzmarshall1278@gmail.com>
2022-01-04 23:46:16 +0100

    Added required attribute

--- pages/transactions/new.vue
+++ pages/transactions/new.vue
@@ -4,16 +4,16 @@
         <form v-on:submit.prevent="addNew">
             <div class="lg:w-2/5 text-center mx-auto py-6 px-8 mt-4 bg-blue-50 rounded-xl">
                 <div class="mt-8">
-                <input name="name" type="date" v-model="date" class="w-full py-4  px-6 pl-3 border-3 border-blue-500 text-gray-400">
+                <input name="name" type="date" :required="true" v-model="date" class="w-full py-4  px-6 pl-3 border-3 border-blue-500 text-gray-400">
                 </div>
                 <div class="mt-8">
-                    <input placeholder="Weight" name="name" v-model="weight" type="number" class="w-full py-4 pl-3 border-3 border-blue-500">
+                    <input placeholder="Weight" name="name" :required="true" v-model="weight" type="number" class="w-full py-4 pl-3 border-3 border-blue-500">
                 </div>
                 <div class="mt-8">
-                    <input placeholder="price" name="name" v-model="price" type="number" class="w-full py-4 pl-3 border-3 border-blue-500"/>
+                    <input placeholder="price" name="name" :required="true" v-model="price" type="number" class="w-full py-4 pl-3 border-3 border-blue-500"/>
                 </div>
                 <div class="mt-8">
-                    <input placeholder="Quantity" name="name" v-model="quantity" type="number" class="w-full py-4 pl-3 border-3 border-blue-500"/>
+                    <input placeholder="Quantity" :required="true" name="name" v-model="quantity" type="number" class="w-full py-4 pl-3 border-3 border-blue-500"/>
                 </div>
                 <div class="mt-8 w-full">
                     <div class="bg-blue-500 py-4 cursor-pointer text-white" @click="showDropDown = true">Search Seller</div>
