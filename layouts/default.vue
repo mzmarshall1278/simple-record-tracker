f17a602b9e9de9098a9826df2dfbe16e8381bf7d
MZ Marshall <mzmarshall1278@gmail.com>
2022-01-20 10:24:27 +0100

    add loading and error components in the default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,27 @@
 <template>
     <div>
+        <loader v-if="loading"/>
+        <error v-if="error.length"/>
+        <success v-if="success != ''"/>
         <Nav/>
         <Nuxt keep-alive/>
     </div>
 </template>
 <script>
 export default {
-
+//   middleware: ['check'],
+  computed: {
+    loading(){
+      return this.$store.state.loading
+    },
+    error(){
+      return this.$store.state.error
+    },
+    success(){
+      return this.$store.state.success;
+    }
+  },
+  
 }
 </script>
 <style>
