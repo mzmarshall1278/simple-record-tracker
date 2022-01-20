753fbd1961843aba9b179d4c8c966947e3f0888b
MZ Marshall <mzmarshall1278@gmail.com>
2022-01-20 10:23:38 +0100

    created an overlay loader

---
+++ components/Loader.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <div class="opacity-90 fixed inset-0 z-40 bg-blue-900 h-screen ">
+      <p class="w-full mt-56 text-center font-bold align-middle inline-block text-white text-4xl animate-bounce">Loading...</p>
+    </div>
+  </div>
+</template>
