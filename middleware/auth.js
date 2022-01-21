246f4ea3bc4fbc4deef7bb97bd140b64c8413ace
MZ Marshall <mzmarshall1278@gmail.com>
2022-01-21 23:07:32 +0100

    temporary commeny

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,9 @@
+import Cookies from 'js-cookie'
+
 export default function ({store, redirect}) {
-    if(!store.getters.isAuthenticated) {
-        redirect('/auth/login');
-    }
+    // if(!store.getters.isAuthenticated) {
+    //     redirect('/auth/login');
+    // }
+
+    const cook = Cookies.get('token');
 }
