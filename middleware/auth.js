b985d33ec65de85363d0cb40bbe61c2c44f98656
MZ Marshall <mzmarshall1278@gmail.com>
2022-01-24 15:49:31 +0100

    removed js-kookie

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,6 @@
-import Cookies from 'js-cookie'
-
 export default function ({store, redirect}) {
     // if(!store.getters.isAuthenticated) {
     //     redirect('/auth/login');
     // }
 
-    const cook = Cookies.get('token');
 }
