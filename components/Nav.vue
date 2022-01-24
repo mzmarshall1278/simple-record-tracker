25cb1c1e45536cad1cc7d29e8be1ced2b089cf6c
MZ Marshall <mzmarshall1278@gmail.com>
2022-01-24 15:49:20 +0100

    added login button

--- components/Nav.vue
+++ components/Nav.vue
@@ -2,13 +2,16 @@
     <div class="w-full bg-blue-900 py-2 px-12 font-normal">
         <nav class="flex justify-between">
             <h1 class="text-white text-3xl w-1/4">Simple<span class="text-blue-400 font-black">ERP</span></h1>
-            <div class="w-2/4 mx-auto">
+            <div class="w-2/4 mx-auto flex justify-between">
                 <ul class="flex justify-between mr-12 text-white text-lg mt-2 p-4">
                     <li><router-link class="hover:bg-white hover:text-blue-400 py-4 px-6 rounded-lg" to="/">Home</router-link></li>
                     <li><router-link class="hover:bg-white hover:text-blue-400 py-4 px-6 rounded-lg" to="/transactions">Transactions</router-link></li>
                     <li><router-link class="hover:bg-white hover:text-blue-400 py-4 px-6 rounded-lg" to="/sellers">Sellers</router-link></li>
                     <!-- <li>stock</li> -->
                 </ul>
+                <div>
+                    <button class="text-white my-auto h-full"><router-link class="hover:bg-white hover:text-blue-400 py-4 px-6 rounded-lg" to="/auth/login">login</router-link></button>
+                </div>
             </div>
         </nav>
     </div>
