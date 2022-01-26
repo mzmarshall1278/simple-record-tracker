21ea438630b00cb5d89ee4cc50b0b78b6d8e9415
MZ Marshall <mzmarshall1278@gmail.com>
2022-01-26 15:47:16 +0100

    added app table and removed cards

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,26 @@
 <template>
   <div>
     <div class="text-5xl font-black text-blue-500 text-center mt-12">INVENTORY</div>
-    <div class="w-10/12 mx-auto mt-4 grid md:grid-cols-2 lg:grid-cols-3 gap-5">
-    <Card :title="stock.title" :rows="stock.rows" />
-    <Card :title="pending.title" :rows="pending.rows" />
-    <Card :title="free.title" :rows="free.rows" />
-
+      <div class="w-10/12 mx-auto">
+        <AppTable
+        :columns="columns"
+        url="/transaction?completed=false"
+        />
     </div>
   </div>
 </template>
 
 <script>
-// import Cookies from 'js-cookie'
 export default { 
-  // middleware: 'auth',
-  // asyncData({$axios}){
-  //   return $axios.$get(`http://localhost:3000/transaction/ongoing`, {
-  //     headers: {
-  //       authorization: `Bearer ${Cookies.get('token')}`
-  //     }
-  //   }).then(res=> {
-  //     console.log(res.length);
-  //   }).catch(err=> {
-  //     console.log(err);
-  //   })
-  // },
+  middleware: 'auth',
   data(){
     return {
-
-    }
-  },
-  computed: {
-    stock(){
-      return this.$store.getters['stock/getStockDetails'];
-    },
-    pending(){
-      return this.$store.getters['stock/getPendingDetails'];
-    },
-    free(){
-      return this.$store.getters['stock/getFreeDetails'];
+      columns: [
+        {label: 'Vendor', value: 'seller', child: 'name'},
+        {label: 'Weight', value: 'weight'},
+        {label: 'Quantity', value: 'quantity'},
+        {label: 'Price', value: 'price'},
+      ],
     }
   }
 }
